9a0b33547dcfa75bad6c68ff543743517eddf5eb
YauheniShakau <zhshakov@gmail.com>
2018-08-17 20:32:28 +0300

    done

--- dir_structure_gen.sh
+++ dir_structure_gen.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 # A bash script to generate a random directory structure with subfolders and files
 
-read -p "Please enter directory /../../DIR : " directory
-read -p "Please enter the depth of nesting: " max_depth
-#read max_size
-read -p "Please enter max amount of files and folders to be created: " max_iterations
+read -p "Please, enter directory /../../DIR : " directory
+read -p "Please, enter the depth of nesting: " max_depth
+read -p "Please, enter max size of a file: " max_size
+read -p "Please, enter max amount of files and folders to be created: " max_items
 
-cd $directory
+mkdir -p /home/eugene/work/bash_scripts/$directory
+cd /home/eugene/work/bash_scripts/$directory
 
-num=1
-nom=1
-nem=1
+depth=$(( ( RANDOM % ( $max_depth - 1 ) ) + 1 ))
 
-depth=$(( ( RANDOM % $max_depth - 2 )  + 3 ))
-iterations=$(( ( RANDOM % $max_iterations - 2)  + 3 ))
+dc=1
+dc1=1
 
-while [ $nem -le $depth ]; do
-	while [ $num -le $iterations ]; do
-		sudo mkdir "$(( RANDOM % 99999999 ))"
-		num=$[ $num + 1 ]
-	done
+while [ $dc1 -le $depth ]; do
 
-	shopt -s nullglob
+	while [ $dc -le $depth ]; do
+
+		if [ $(( $( ls -l | wc -l ) - 1 )) -eq 0 ]
+		then
+			items=$(( ( RANDOM % ( $max_items - 1 ) ) + 1 ));
+			for i in `seq "$items"`; do mkdir -p "$(( RANDOM % 99999999 ))"; done;
+			items=$(( ( RANDOM % ( $max_items - 1 ) ) + 1 ));
+			for i in `seq "$items"`; do touch "$(( RANDOM % 99999999 ))".md; done;
+		fi
+
 	dirs=(*/)
-	[[ $dirs ]] && cd -- "${dirs[RANDOM%${#dirs[@]}]}"
+	cd "${dirs[RANDOM%${#dirs[@]}]}"
+	dc=$[ $dc + 1 ]
 
-	while [ $nom -le $iterations ]; do
-		sudo touch "$(( RANDOM % 99999999 ))".md
-		nom=$[ $nom + 1 ]
-	done
-	nem=$[ $nem + 1 ]
+    done
+
+    depth=$(( ( RANDOM % ( $max_depth - 1 ) ) + 1 ))
+    cd /home/eugene/work/bash_scripts/$directory
+    dc1=$[ $dc1 + 1 ]
+    dc=1
+
 done
